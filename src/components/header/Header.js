b25014b9a5f7294c4806462615a5e137b12ae37a
Donatien LORET <donatienloret@gmail.com>
2017-05-26 08:56:14 +0200

    Header date and time implementation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,11 +4,25 @@ import logo from '../../assets/images/companyLogo.png';
 import './Header.css';
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateHour: new Date().toString()
+    }
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        dateHour: new Date().toString()
+      })
+    }, 1000);
+  }
   render () {
     return (
       <header>
         <img src={logo} alt='company logo'/>
         <h1>Support team webpages</h1>
+        <h2>{this.state.dateHour}</h2>
       </header>
     );
   }
