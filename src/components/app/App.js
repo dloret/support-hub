3bfd54d406fe35602211376da5bd2048c645f3dc
Donatien LORET <donatienloret@gmail.com>
2017-05-27 09:59:04 +0200

    nav styling and animation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,15 +8,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      menuList: [...new Set(dataSource.map((link) => link.context))]
+      menuList: [...new Set(dataSource.map((link) => link.context))],
+      selectedMenu: ''
     }
   }
+
+  componentWillMount() {
+    this.setState({
+      selectedMenu: this.state.menuList[0]
+    });
+  }
+
+  updateSelectedMenu = (menu) => this.setState({
+    selectedMenu: menu
+  })
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Nav menus={this.state.menuList} />
+        <Nav menus={this.state.menuList}
+          selectedMenu={this.state.selectedMenu}
+          updateMenu={this.updateSelectedMenu} />
       </div>
     );
   }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import './Nav.css';
 
-export const Nav = ({menus}) => {
+export const Nav = ({menus, selectedMenu, updateMenu}) => {
+  const changeMenu = (menu) => updateMenu(menu);
+
   return (
     <nav>
       <ul>
-        {menus.map(menu => <li key={menu}>{menu}</li>)}
+        {menus.map(menu => {
+          return (
+            <li
+              key={menu}
+              onClick={changeMenu.bind(null, menu)}
+              className={(menu === selectedMenu) ? 'active' : null}
+            >{menu}</li>
+          );
+        })}
       </ul>
     </nav>
   );
